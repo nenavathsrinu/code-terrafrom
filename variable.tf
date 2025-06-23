ef9a448458f2d8db2cfa46b822de1018bd19074f
srinu <nenavath2013@gmail.com>
2025-06-23 16:23:28 +0530

    dev

---
+++ variable.tf
@@ -0,0 +1,30 @@
+variable "region" {
+  type = string
+}
+
+variable "cidr" {
+  type = string
+
+}
+
+variable "publicsubnet" {
+  type = list(string)
+
+}
+
+variable "privatesubnet" {
+  type = list(string)
+}
+
+variable "tags" {
+  description = "map to each resource"
+  type        = map(string)
+  default = {
+    Env             = "dev"
+    Department      = "could"
+    resource_owner  = "srinu_naik"
+    resource_owner2 = "hcl"
+  }
+
+
+}
